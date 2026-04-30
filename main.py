4d414e56ad0c74da26cda8ebb74c428432146a48
Martin Smola <info@cleverit.cz>
2026-04-30 18:25:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -566,13 +566,14 @@ def main(disable_exit=False):
                 output_filename = 'ESET ACCOUNTS.txt'
                 if args['key'] or args['small_business_key'] or args['vpn_codes']:
                     output_filename = 'ESET KEYS.txt'
-                    with open('ESET KEYS.txt', 'r') as f:
-                        print(f.read())
+
                     EK_obj = EK(email_obj, DRIVER, 'ESET HOME' if args['key'] else 'SMALL BUSINESS')
                     print(EK_obj)
                     
                     EK_obj.sendRequestForKey()
+                    
                     l_name, l_key, l_out_date = EK_obj.getLD()
+                    print(output_line)
                     output_line = '\n'.join([
                         '',
                         '-------------------------------------------------',
